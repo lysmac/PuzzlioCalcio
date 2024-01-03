64da0dc3dd55212ecb9805810f474862cadea59f
edvindjulic <djulicedvin@gmail.com>
2024-01-04 00:37:00 +0100

    Size change on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import logo from "../../public/PClogo.svg"
 export default function Header() {
   return (
     <div className="flex flex-col justify-center items-center gap-2 w-full py-8 sm:pt-20 pb-8  bg-dt-bg">
-      <img src={logo} alt="Logo" className="w-40" />
+      <img src={logo} alt="Logo" className="w-32 sm:w-40" />
       <div>
         <p className="text-sm text-white ">
           Guess the footballer in as few tries as possible{" "}
