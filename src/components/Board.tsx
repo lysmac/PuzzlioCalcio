06cf061d354964841af0922cd8a8e4c87878fb24
Carl Hasselblad <carl.hasselblad@wardt.com>
2024-01-03 13:59:15 +0100

    small size adjustment

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -14,7 +14,7 @@ export default function Board() {
       <div className="outline bg-slate-400 justify-center items-center flex gap-2 flex-col">
         <div className="flex w-full justify-center gap-2">
           {wordArray.map((letter) => (
-            <Tile letter={letter} place="right" />
+            <Tile letter={letter} place="close" />
           ))}
         </div>{" "}
         <div className="flex w-full gap-1 lg:gap-2">
