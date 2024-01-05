478f7400e52f8305f5a88cb7e8e027c6dd700aed
Carl Hasselblad <carl.hasselblad@wardt.com>
2024-01-05 14:42:20 +0100

    changed svg to a component and added tailwind colors instead. Logo can now change with darkmode too

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import logo from "../../public/PClogo.svg";
+import Logo from "./Logo";
 import Menu from "./menu/Menu";
 
 export default function Header() {
   return (
     <div className="flex flex-col justify-center items-center gap-2 w-full py-8 sm:pt-20 pb-8">
-      <img src={logo} alt="Logo" className="w-32 sm:w-40" />
+      <Logo />
       <div>
         <p className="text-sm ">
           Guess the footballer in as few tries as possible{" "}
