3ea8711ef40550e8b8521ad030d3b0d6da5c9065
edvindjulic <djulicedvin@gmail.com>
2024-01-17 20:24:35 +0100

    primary-text used in Logo. Removed code for darkMode toggle in Menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -5,29 +5,6 @@ import SettingsModal from "./menu-settings/SettingsModal";
 
 export default function Menu() {
   const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
-  /*   function darkModeToggle() {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-    }
-  }
-
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
-    }
-  }, []); */
-
   const { newGame } = useContext(PlayerContext);
 
   return (
