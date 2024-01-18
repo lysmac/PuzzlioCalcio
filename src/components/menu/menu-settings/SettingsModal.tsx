541b2b6d1af625811d43de2707ff1fc7403481fc
edvindjulic <djulicedvin@gmail.com>
2024-01-18 14:23:23 +0100

    Height change, removed md:max-w-lg, removed absolute on close

--- src/components/menu/menu-settings/SettingsModal.tsx
+++ src/components/menu/menu-settings/SettingsModal.tsx
@@ -17,7 +17,7 @@ export default function SettingsModal({ open, onClose }: SettingsModalProps) {
       aria-describedby="Settings modal"
       hideBackdrop
     >
-      <div className="absolute bg-primary-bg top-60 left-1/2 transform -translate-x-1/2 border border-primary-contrast h-[350px] w-full sm:max-w-lg md:max-w-lg">
+      <div className="absolute bg-primary-bg top-60 left-1/2 transform -translate-x-1/2 border border-primary-contrast w-full sm:max-w-lg">
         <div className="flex flex-col justify-center">
           <div className="flex flex-row justify-center items-center relative p-1">
             <p className="text-lg font-bold">Settings</p>
@@ -40,7 +40,7 @@ export default function SettingsModal({ open, onClose }: SettingsModalProps) {
               <p>Placement of delete</p>
               <p>Toggle for delete</p>
             </div>
-            <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center p-1">
+            <div className="flex justify-center items-center p-1">
               <button onClick={onClose} className="font-bold">
                 Close
               </button>
