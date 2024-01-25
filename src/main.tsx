16940fb49df961b7894a3203bee19e81c356d6fc
Carl Hasselblad <carl.hasselblad@wardt.com>
2024-01-25 19:15:14 +0100

    removed unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,15 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import TailwindColors from "./pages/TailwindColors.tsx";
-import Test from "./pages/Test.tsx";
+
 import PlayerProvider from "./PlayerContext.tsx";
 import ThemeProvider from "./ThemeContext.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  { path: "/test", element: <Test /> },
-  { path: "/tailwind", element: <TailwindColors /> },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <ThemeProvider>
     <PlayerProvider>
-      <RouterProvider router={router} />
+      <App />
     </PlayerProvider>
   </ThemeProvider>
   // </React.StrictMode>
--- src/pages/Test.tsx
+++
@@ -1,9 +0,0 @@
-export default function Test() {
-  return (
-    <>
-      <div className="flex justify-center items-center flex-col">
-        <h1 className="text-5xl font-bold underline text-dt-bg">Test</h1>
-      </div>
-    </>
-  );
-}
