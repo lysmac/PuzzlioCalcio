a27936888ce30617f0a1b62d881a0f85b059457d
edvindjulic <djulicedvin@gmail.com>
2024-01-25 01:50:56 +0100

    Modal made smaller. Displays name and club. Conditional rendering on isGameWon.

--- src/components/PlayerModal.tsx
+++ src/components/PlayerModal.tsx
@@ -9,7 +9,7 @@ interface PlayerModalProps {
 }
 
 export default function PlayerModal({ open, onClose }: PlayerModalProps) {
-  const { player } = useContext(PlayerContext);
+  const { player, isGameWon } = useContext(PlayerContext);
   const playerName = player?.name.replace(/ /g, "-").toLowerCase();
   const playerUrl = `https://www.transfermarkt.co.uk/${playerName}/profil/spieler/${player?.id}`;
 
@@ -20,20 +20,25 @@ export default function PlayerModal({ open, onClose }: PlayerModalProps) {
       aria-labelledby="Player modal"
       aria-describedby="Player modal"
     >
-      <div className="absolute bg-primary-bg top-40 left-1/2 transform -translate-x-1/2 border border-primary-contrast w-80 sm:w-96">
+      <div className="absolute bg-primary-bg top-16 left-1/2 transform -translate-x-1/2 border border-primary-contrast w-80 sm:w-96">
         <div className="flex flex-col justify-center">
           <div className="flex flex-row justify-center items-center relative p-1">
-            <p className="text-lg font-bold">Player</p>
+            <p className="text-lg font-bold">
+              {isGameWon ? "You won!" : "You lost!"}
+            </p>
             <button onClick={onClose} className="absolute right-1">
               <IoClose size={30} />
             </button>
           </div>
           <div className="flex flex-col items-center justify-center gap-2">
             <div>
+              <p>The answer was:</p>
+            </div>
+            <div className="flex flex-row gap-4">
               <p>{player?.name}</p>
-              <p>{player?.position}</p>
-              <p>{player?.nationality?.join(", ")}</p>
               <p>{player?.club}</p>
+            </div>
+            <div className="pb-2">
               <a
                 href={playerUrl}
                 target="_blank"
@@ -43,11 +48,6 @@ export default function PlayerModal({ open, onClose }: PlayerModalProps) {
                 Read more about the player
               </a>
             </div>
-          </div>
-          <div className="flex justify-center items-center py-1">
-            <button onClick={onClose} className="font-bold">
-              Close
-            </button>
           </div>
         </div>
       </div>
